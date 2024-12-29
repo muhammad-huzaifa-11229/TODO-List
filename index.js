ebfea865bf17ad41d78fe1db084d610e058784a4
Muhammad Huzaifa <muhammadhuzaifa3112541@gmail.com>
2024-12-29 11:12:51 +0500

    TODO List
    TODO List-1

---
+++ index.js
@@ -0,0 +1,41 @@
+
+import inquirer from "inquirer";
+async function main() {
+    let todos = [];
+    let loop = true;
+    while (loop) {
+        const answers = await inquirer.prompt([
+            {
+                type: "input",
+                name: "TODO",
+                message: "What do you want to add to your todo? "
+            },
+            {
+                type: "confirm",
+                name: "addmore",
+                message: "Do you want to add more todo? ",
+                default: false
+            }
+        ]);
+        const { TODO, addmore } = answers;
+        console.log(answers);
+        loop = addmore;
+        if (TODO) {
+            todos.push(TODO);
+        }
+        else {
+            console.log("Kindly add valid input");
+        }
+    }
+    console.log(todos);
+    if (todos.length > 0) {
+        console.log("Your Todo List: \n");
+        todos.forEach(todo => {
+            console.log(todo);
+        });
+    }
+    else {
+        console.log("No todos found");
+    }
+}
+main().catch(err => console.error(err));
